b52650e59bc603fc2f7aae16fbcff47d6be513b9
Gustavoand39 <111763968+Gustavoand39@users.noreply.github.com>
2023-05-10 14:24:21 -0600

    Implementación login y correcciones generales

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/* Index */
+const email = document.getElementById('login_email');
+const pass = document.getElementById('login_pass');
+const btn = document.getElementById('login_btn');
+const showPass = document.getElementById('showPass');
+const btn_sPass = document.getElementById('btn_sPass');
+
+// Función para mostrar u ocultar la contraseña
+function handdlerPassword(){
+    if(pass.type === 'password'){
+        pass.type = 'text';
+        showPass.classList.remove('fa-eye');
+        showPass.classList.add('fa-eye-slash');
+        btn_sPass.classList.remove('btn-outline-secondary');
+        btn_sPass.classList.add('btn-dark');
+    } else {
+        pass.type = 'password';
+        showPass.classList.add('fa-eye');
+        showPass.classList.remove('fa-eye-slash');
+        btn_sPass.classList.add('btn-outline-secondary');
+        btn_sPass.classList.remove('btn-dark');
+    }
+}
